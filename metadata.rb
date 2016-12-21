1212f6fb3d05f352c9a312f5a1517619666150bb
Gerald L. Hevener Jr <ghevener@customink.com>
2016-12-21 18:28:19 -0500

    Fix foodcritic failures FC064 & FC065.

--- metadata.rb
+++ metadata.rb
@@ -4,4 +4,6 @@ maintainer_email 'jackl0phty@gmail.com'
 license          'Apache 2.0'
 description      'Installs/Configures chefknife'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+issues_url 'https://github.com/jackl0phty/opschef-cookbook-chefknife/issues' if respond_to?(:issues_url)
+source_url 'https://github.com/jackl0phty/opschef-cookbook-chefknife' if respond_to?(:source_url)
 version          '0.1.25'
