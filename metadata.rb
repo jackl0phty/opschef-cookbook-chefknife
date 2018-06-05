d5cce261fab9b983f70860e2970c3a99edd90cd4
Gerald L. Hevener Jr <ghevener@customink.com>
2018-06-05 13:53:19 -0400

    Address foodcritic failures.

--- metadata.rb
+++ metadata.rb
@@ -7,3 +7,6 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 issues_url 'https://github.com/jackl0phty/opschef-cookbook-chefknife/issues' if respond_to?(:issues_url)
 source_url 'https://github.com/jackl0phty/opschef-cookbook-chefknife' if respond_to?(:source_url)
 version          '0.1.29'
+chef_version '>= 12.5' if respond_to?(:chef_version)
+supports 'ubuntu'
+license 'Apache-2.0'
